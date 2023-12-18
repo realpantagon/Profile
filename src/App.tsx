7b9b91ecc03c696dbf0ee2cf011e13f15a16b5a2
Relpantagon <pichayoothunchainao@gmail.com>
2023-12-19 00:40:46 +0700

    navbar template from primereact

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
-import { Button } from "primereact/button";
+import navbar from "../Component/Navbar";
+import Navbar from "../Component/Navbar";
 function App() {
   return (
     <div>
-      <div className="flex flex-column md:flex-row justify-content-between my-5">
-        <Button
-          type="button"
-          label="Button 1"
-          className="mb-3 md:mb-0"
-        ></Button>
-        <Button
-          type="button"
-          label="Button 2"
-          className="p-button-secondary mb-3 md:mb-0"
-        ></Button>
-        <Button
-          type="button"
-          label="Button 3"
-          className="p-button-help"
-        ></Button>
-      </div>
-      <div className="flex flex-column md:flex-row justify-content-between my-5"></div>
-      <div className=""> สวัสดีครับ</div>
+      <Navbar/>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { PrimeReactProvider } from 'primereact/api';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { PrimeReactProvider } from "primereact/api";
 import "primereact/resources/themes/lara-dark-teal/theme.css";
 
+import { NextUIProvider } from "@nextui-org/react";
 
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  
-    <PrimeReactProvider>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-    </PrimeReactProvider>
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <PrimeReactProvider>
+    <NextUIProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </NextUIProvider>
+  </PrimeReactProvider>
+);
